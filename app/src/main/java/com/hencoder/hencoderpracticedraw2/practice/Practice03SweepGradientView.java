ab816fee3663f1d56f8ade63071345a1e59ef6cd
Jucongyuan <jucongyuan@JucongyuandeMacBook-Pro.local>
2017-09-26 14:09:09 +0800

    完成

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice03SweepGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice03SweepGradientView.java
@@ -3,6 +3,8 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -25,12 +27,12 @@ public class Practice03SweepGradientView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 SweepGradient
         // SweepGradient 的参数：圆心坐标：(300, 300)；颜色：#E91E63 到 #2196F3
+        paint.setShader(new SweepGradient(300, 300, 0xFFE91E63, 0xFF2196F3));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         canvas.drawCircle(300, 300, 200, paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice14MaskFilterView.java
@@ -3,9 +3,9 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,6 +15,10 @@ import com.hencoder.hencoderpracticedraw2.R;
 public class Practice14MaskFilterView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     Bitmap bitmap;
+    private BlurMaskFilter normalBlurMaskFilter;
+    private BlurMaskFilter innerBlurMaskFilter;
+    private BlurMaskFilter outerBlurMaskFilter;
+    private BlurMaskFilter solidBlurMaskFilter;
 
     public Practice14MaskFilterView(Context context) {
         super(context);
@@ -28,9 +32,16 @@ public class Practice14MaskFilterView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+
     {
         setLayerType(LAYER_TYPE_SOFTWARE, null);
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.what_the_fuck);
+
+        normalBlurMaskFilter = new BlurMaskFilter(50, BlurMaskFilter.Blur.NORMAL);
+        innerBlurMaskFilter = new BlurMaskFilter(50, BlurMaskFilter.Blur.INNER);
+        outerBlurMaskFilter = new BlurMaskFilter(50, BlurMaskFilter.Blur.OUTER);
+        solidBlurMaskFilter = new BlurMaskFilter(50, BlurMaskFilter.Blur.SOLID);
+
     }
 
     @Override
@@ -40,15 +51,19 @@ public class Practice14MaskFilterView extends View {
         // 用 Paint.setMaskFilter 来设置不同的 BlurMaskFilter
 
         // 第一个：NORMAL
+        paint.setMaskFilter(normalBlurMaskFilter);
         canvas.drawBitmap(bitmap, 100, 50, paint);
 
         // 第二个：INNER
+        paint.setMaskFilter(innerBlurMaskFilter);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, 50, paint);
 
         // 第三个：OUTER
+        paint.setMaskFilter(outerBlurMaskFilter);
         canvas.drawBitmap(bitmap, 100, bitmap.getHeight() + 100, paint);
 
         // 第四个：SOLID
+        paint.setMaskFilter(solidBlurMaskFilter);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 200, bitmap.getHeight() + 100, paint);
     }
 }
